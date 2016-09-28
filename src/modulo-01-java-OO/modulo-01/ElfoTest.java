2f4b3fa7391b537dff5c3a6e5cdb1db682178f47
Unknown <jonathanwssantos1@gmail.com>
2016-09-27 23:15:09 -0300

    test(elfo): realizar novos testes sobre atirar flechas.

--- src/modulo-01-java-OO/modulo-01/ElfoTest.java
+++ src/modulo-01-java-OO/modulo-01/ElfoTest.java
@@ -3,6 +3,7 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import java.util.Random;
 
 public class ElfoTest
 {
@@ -62,5 +63,30 @@ public class ElfoTest
         e.cacarAnao(11, d);
         
         assert(d.getPontosDeVida()<=0);
+    }
+    
+    //caso o nº de flechas seja igual a um numero gerado aleatoriamente o dano será crítico
+    @Test
+    public void danoCritico(){
+        Random r = new Random();
+        
+        Elfo f = new Elfo("churumidas");
+        
+        f.atirarFlecha(2);
+        
+        int random = r.nextInt(10);
+        
+        assertEquals(random, f.getNumFlechas());
+    }
+    
+    @Test
+    public void atirarUmaFlecha(){
+        Elfo g = new Elfo("night");
+        
+        int numFlechas = 1;
+        g.atirarFlecha(numFlechas);
+        
+        assertEquals(1, g.getNumFlechas());
+        
     }
 }
