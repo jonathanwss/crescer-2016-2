6afc68aac7ec8a3ab9433a7edbff5a53d4c4afd7
Unknown <jonathanwssantos1@gmail.com>
2016-10-04 23:33:08 -0300

    feat(Exercito): criar a classe Exercito e implementar método alistar elfos e buscar elfos.

---
+++ src/modulo-01-java-OO/modulo-01/Exercito.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Exercito
+{
+    ArrayList<Elfo> exercito = new ArrayList<>();
+    public void alistarUmEmfo(Elfo e){
+        exercito.add(e);
+    }
+
+    public ArrayList<Elfo> getExercito(){
+        return exercito;
+    }
+    
+    public Elfo buscarElfoPorNome(Elfo e){
+        int cont = exercito.size();
+        Elfo elfoEncontrado = null;
+        for(int x = 0; x < cont; x++){
+            if(exercito.get(x).getNome() == e.getNome()){
+                elfoEncontrado = exercito.get(x);
+                break;
+                }
+        }
+        return elfoEncontrado;
+    }
+}
---
+++ src/modulo-01-java-OO/modulo-01/ExercitoTest.java
@@ -0,0 +1,98 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class ExercitoTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class ExercitoTest
+{
+    @Test
+    public void adicionarElfoVerde(){
+        Exercito bimtz = new Exercito();
+        ElfoVerde e = new ElfoVerde("Elf");
+        bimtz.alistarUmEmfo(e) ;
+        
+        boolean deuCerto = bimtz.getExercito().get(0).getNome() == e.getNome();
+        
+        assertTrue(deuCerto);
+    }
+    
+    @Test
+    public void adicionarElfoNoturno(){
+        Exercito bimtz = new Exercito();
+        ElfoNoturno e = new ElfoNoturno("Dark");
+        bimtz.alistarUmEmfo(e) ;
+        
+        boolean deuCerto = bimtz.getExercito().get(0).getNome() == e.getNome();
+        
+        assertTrue(deuCerto);
+    }
+    
+    @Test
+    public void adicionarNovoElfoElfoNoturno(){
+        Exercito bimtz = new Exercito();
+        ElfoNoturno e = new ElfoNoturno("Dark");
+        bimtz.alistarUmEmfo(e) ;
+        
+        boolean deuCerto = bimtz.getExercito().get(0).getNome() == new Elfo("Dark").getNome();
+        
+        assertTrue(deuCerto);
+    }
+    
+    @Test
+    public void adicionarElfoNoturnoEElfoVerdeAoExercito(){
+        Exercito bimtz = new Exercito();
+        ElfoNoturno dark = new ElfoNoturno("Dark");
+        ElfoVerde green = new ElfoVerde("Elf");
+        bimtz.alistarUmEmfo(green) ;
+        bimtz.alistarUmEmfo(dark) ;
+        
+        boolean deuCerto = bimtz.getExercito().get(0).getNome() == green.getNome() && bimtz.getExercito().get(1).getNome() == dark.getNome();
+        
+        assertTrue(deuCerto);
+    }
+    
+    @Test
+    public void buscarElfoPorNomeAndEncontrar(){
+        Exercito bimtz = new Exercito();
+        ElfoNoturno e = new ElfoNoturno("Dark");
+        bimtz.alistarUmEmfo(e) ;
+        
+        boolean encontrou = bimtz.buscarElfoPorNome(e) != null;
+        
+        assertTrue(encontrou);
+    }
+    
+    @Test
+    public void buscarElfoPorNomeAndNaoEncontrar(){
+        Exercito bimtz = new Exercito();
+        ElfoNoturno e = new ElfoNoturno("Donnie Darko");
+        ElfoNoturno e1 = new ElfoNoturno("Darko");
+        bimtz.alistarUmEmfo(e) ;
+        
+        boolean naoEncontrou = bimtz.buscarElfoPorNome(e1) == null;
+        
+        assertTrue(naoEncontrou);
+    }
+   
+    @Test
+    public void buscarElfoPorNomeCom2ElfosComNomeIgual(){
+        Exercito bimtz = new Exercito();
+        ElfoNoturno e = new ElfoNoturno("Dark");
+        ElfoVerde green = new ElfoVerde("Dark");
+        bimtz.alistarUmEmfo(e) ;
+        bimtz.alistarUmEmfo(green) ;
+        
+        boolean encontrou = bimtz.buscarElfoPorNome(new Elfo("Dark")) != null;
+        
+        assertTrue(encontrou);
+    }
+    
+}
