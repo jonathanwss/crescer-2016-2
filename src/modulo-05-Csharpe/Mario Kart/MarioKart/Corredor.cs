7496d6c570806a1579896420bc4a38ba59e8fc74
Unknown <jonathanwssantos1@gmail.com>
2016-10-27 17:04:12 -0200

    feat(C#): adicionar projeto mario kart e classe Corredor com teste.

---
+++ src/modulo-05-Csharpe/Mario Kart/MarioKart/Corredor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart
+{
+    public class Corredor
+    {
+        public Corredor(String nome, NivelHabilidadeCorredor nivel)
+        {
+            this.Nome = nome;
+            this.NivelHabilidade = nivel;
+        }
+
+        public String Nome { get; }
+
+        public NivelHabilidadeCorredor NivelHabilidade { get; }
+    }
+}
---
+++ src/modulo-05-Csharpe/Mario Kart/MarioKartTest/CorredorTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MarioKart;
+
+namespace MarioKartTest
+{
+    [TestClass]
+    public class CorredorTest
+    {
+        [TestMethod]
+        public void CorredorEhCriadoComNomeENivelDeHabilidade()
+        {
+            Corredor corredor = new Corredor("José", NivelHabilidadeCorredor.Noob);
+
+            Assert.AreEqual("José", corredor.Nome);
+            Assert.AreEqual(NivelHabilidadeCorredor.Noob, corredor.NivelHabilidade);
+        }
+    }
+}
