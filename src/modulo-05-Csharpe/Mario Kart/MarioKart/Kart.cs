db3acbbc05b12ca705b13511dfc59343a61a8a78
Unknown <jonathanwssantos1@gmail.com>
2016-10-27 17:22:04 -0200

    feat(C#): adicionar classe abstrata Kart.

---
+++ src/modulo-05-Csharpe/Mario Kart/MarioKart/Kart.cs
@@ -0,0 +1,23 @@
+﻿using MarioKart.Equipamento;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart
+{
+    public abstract class Kart
+    {
+        public Kart(Corredor piloto)
+        {
+            this.Piloto = piloto;
+        }
+
+        protected Corredor Piloto { get; set; }
+
+        protected List<IEquipamento> Equipamentos { get; }
+
+        protected int Velocidade { get; }
+    }
+}
