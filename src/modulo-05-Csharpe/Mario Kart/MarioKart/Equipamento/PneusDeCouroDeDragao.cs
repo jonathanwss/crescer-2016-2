6ce8a99b465c84ddee93f3b17ac09f2a83b9c866
Unknown <jonathanwssantos1@gmail.com>
2016-10-27 17:20:35 -0200

    feat(C#): adicionar classes equipamentos nivel 1.

---
+++ src/modulo-05-Csharpe/Mario Kart/MarioKart/Equipamento/PneusDeCouroDeDragao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart.Equipamento
+{
+    public class PneusDeCouroDeDragao : IEquipamento
+    {
+        public int Bonus
+        {
+            get
+            {
+                return 2;
+            }
+        }
+    }
+}
