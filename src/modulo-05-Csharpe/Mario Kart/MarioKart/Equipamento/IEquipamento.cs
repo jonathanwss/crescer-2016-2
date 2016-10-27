7d191cb41f135a14003baa6d4f4c92e9368bd0d9
Unknown <jonathanwssantos1@gmail.com>
2016-10-27 17:21:29 -0200

    feat(C#): adicionar iterface IEquipameto.

---
+++ src/modulo-05-Csharpe/Mario Kart/MarioKart/Equipamento/IEquipamento.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart.Equipamento
+{
+    public interface IEquipamento
+    {
+        int Bonus { get; }
+    }
+}
