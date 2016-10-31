ea124fd576b779dc61f267cc1dfc17ea9bbd8f3d
Unknown <jonathanwssantos1@gmail.com>
2016-10-31 17:29:09 -0200

    feat(C#): adicionar inicio exercicio Street Fighter.

---
+++ src/modulo-05-Csharpe/StreetFighter.Web/StreetFighter.Web/Controllers/StreetFighterController.cs
@@ -0,0 +1,41 @@
+﻿using StreetFighter.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StreetFighter.Web.Controllers
+{
+    public class StreetFighterController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FichaTecnica()
+        {
+            var modelFichaTecnica = new FichaTecnicaModel();
+
+            modelFichaTecnica.PrimeiraAparicao = "Street Fighter II The World Warrior (1991)";
+            modelFichaTecnica.DataNascimento = new DateTime(12/02/1966);
+            modelFichaTecnica.Altura = 192;
+            modelFichaTecnica.Peso = 96;
+            modelFichaTecnica.TipoSanguineo = "B";
+            modelFichaTecnica.HabilidadesEspeciais = "Caçar, Eletricidade.";
+            modelFichaTecnica.Gosta = "Frutas tropicais, Pirarucu, Sua mãe.";
+            modelFichaTecnica.Desgosta = "Army ants (espécie de formiga).";
+            modelFichaTecnica.EstiloDeLuta = "Luta Selvagem autodidata (Army Ants) / Capoeira.";
+            modelFichaTecnica.Origem = "Brasil (lugar de nascença é provável como sendo Tailândia).";
+            modelFichaTecnica.FalaVitoria = "'Ver você em ação é uma piada!'.";
+            modelFichaTecnica.ssf2NickName = "'A selvagem criança da natureza'.";
+            modelFichaTecnica.sa3NickName = "'A animal pessoa amazônica'.";
+            modelFichaTecnica.sf4NickName = "'Guerreiro da selva'.";
+            modelFichaTecnica.sfa3Stage = "Ramificação do Rio Madeira - pantano, Brasil (ramificação do rio Madeira: talvez possa ser Mato Grosso, ou Tocantins?).";
+            modelFichaTecnica.sf2Stage = "Bacia do rio Amazonas (Brasil).";
+            modelFichaTecnica.GolpesEspeciais = "Electric Thunder, Rolling Attack.";
+            return View(modelFichaTecnica);
+        }
+    }
+}
---
+++ src/modulo-05-Csharpe/StreetFighter.Web/StreetFighter.Web/Models/FichaTecnicaModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StreetFighter.Web.Models
+{
+    public class FichaTecnicaModel
+    {
+        public string PrimeiraAparicao { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public int Altura { get; set; }
+        public double Peso { get; set; }
+        public string Medidas { get; set; }
+        public string TipoSanguineo { get; set; }
+        public string HabilidadesEspeciais { get; set; }
+        public string Gosta { get; set; }
+        public string Desgosta { get; set; }
+        public string EstiloDeLuta { get; set; }
+        public string Origem { get; set; }
+        public string FalaVitoria { get; set; }
+        public string ssf2NickName { get; set; }
+        public string sa3NickName { get; set; }
+        public string sf4NickName { get; set; }
+        public string sfa3Stage { get; set; }
+        public string sf2Stage { get; set; }
+        public string GolpesEspeciais { get; set; }
+
+    }
+}
