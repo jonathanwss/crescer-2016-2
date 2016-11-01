e1c9b6dd0e65f45348bb6496bee228c46a7e9608
Unknown <jonathanwssantos1@gmail.com>
2016-10-31 23:29:49 -0200

    feat(C#): adicionar exercicio Street Fighter Web.

--- src/modulo-05-Csharpe/StreetFighter.Web/StreetFighter.Web/Controllers/StreetFighterController.cs
+++ src/modulo-05-Csharpe/StreetFighter.Web/StreetFighter.Web/Controllers/StreetFighterController.cs
@@ -19,9 +19,10 @@ namespace StreetFighter.Web.Controllers
             var modelFichaTecnica = new FichaTecnicaModel();
 
             modelFichaTecnica.PrimeiraAparicao = "Street Fighter II The World Warrior (1991)";
-            modelFichaTecnica.DataNascimento = new DateTime(12/02/1966);
+            modelFichaTecnica.DataNascimento = new DateTime(1996, 2, 12).ToLocalTime();
             modelFichaTecnica.Altura = 192;
             modelFichaTecnica.Peso = 96;
+            modelFichaTecnica.Medidas = "B198, C120, Q172";
             modelFichaTecnica.TipoSanguineo = "B";
             modelFichaTecnica.HabilidadesEspeciais = "Caçar, Eletricidade.";
             modelFichaTecnica.Gosta = "Frutas tropicais, Pirarucu, Sua mãe.";
@@ -35,6 +36,31 @@ namespace StreetFighter.Web.Controllers
             modelFichaTecnica.sfa3Stage = "Ramificação do Rio Madeira - pantano, Brasil (ramificação do rio Madeira: talvez possa ser Mato Grosso, ou Tocantins?).";
             modelFichaTecnica.sf2Stage = "Bacia do rio Amazonas (Brasil).";
             modelFichaTecnica.GolpesEspeciais = "Electric Thunder, Rolling Attack.";
+            return View(modelFichaTecnica);
+        }
+
+        public ActionResult Sobre()
+        {
+            var modelFichaTecnica = new FichaTecnicaModel();
+
+            modelFichaTecnica.PrimeiraAparicao = "Hospital Centenario (1998)";
+            modelFichaTecnica.DataNascimento = new DateTime(1998, 9, 17).ToLocalTime();
+            modelFichaTecnica.Altura = 180;
+            modelFichaTecnica.Peso = 82;
+            modelFichaTecnica.Medidas = "B198, C120, Q172";
+            modelFichaTecnica.TipoSanguineo = "O";
+            modelFichaTecnica.HabilidadesEspeciais = "De acordo com os outros 'Sabe fazer coisas no computador'";
+            modelFichaTecnica.Gosta = "Just a few things";
+            modelFichaTecnica.Desgosta = "Quase tudo";
+            modelFichaTecnica.EstiloDeLuta = "Speed Run";
+            modelFichaTecnica.Origem = "Brasil (lugar de nascença é provável como São Leo)";
+            modelFichaTecnica.FalaVitoria = "HAHA LEGEN... espera um pouquinho ...DARIO!";
+            modelFichaTecnica.ssf2NickName = "A selvagem criança da natureza(aham sei)";
+            modelFichaTecnica.sa3NickName = "A animal pessoa amazônica(animal talvez)";
+            modelFichaTecnica.sf4NickName = "Guerreiro da selva(tão tá né)";
+            modelFichaTecnica.sfa3Stage = "Ramificação do Arroio Gauchinho - Brasil (Divisa são leo com NH)";
+            modelFichaTecnica.sf2Stage = "Dick do rio dos Sinos (Brasil)";
+            modelFichaTecnica.GolpesEspeciais = "unknown(até por ele mesmo)";
             return View(modelFichaTecnica);
         }
     }
