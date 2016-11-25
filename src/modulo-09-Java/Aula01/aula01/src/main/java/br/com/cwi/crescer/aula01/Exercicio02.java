25c34adb0b00b419b4b9850839f9d7b84a5aaa3c
Unknown <jonathanwssantos1@gmail.com>
2016-11-25 14:06:06 -0200

    feat(Java): adicionar exercicio 2 aula 1.

--- src/modulo-09-Java/Aula01/aula01/src/main/java/br/com/cwi/crescer/aula01/Exercicio02.java
+++ src/modulo-09-Java/Aula01/aula01/src/main/java/br/com/cwi/crescer/aula01/Exercicio02.java
@@ -8,7 +8,6 @@ package br.com.cwi.crescer.aula01;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.time.ZonedDateTime;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -18,25 +17,43 @@ import java.util.Date;
  */
 public class Exercicio02 {
     public static void main(String ars[]){
-        String data = "25/11/2015";
+        String data = "24/11/2015";
         SimpleDateFormat formato = new SimpleDateFormat("MM/yyyy");
         try{
-            long diasPassou = TempoDecorrido(data);
-           
-            System.out.println(diasPassou);
+            long diasPassou = TempoDecorridoEmDias(data);
+            String dataFinal = TempoDecorridoEmFormatoData(diasPassou);
+            System.out.println(dataFinal);
         }catch(ParseException e){
             System.out.println(e);
         }
     }
     
-    public static long TempoDecorrido(String data) throws ParseException{
+    public static long TempoDecorridoEmDias(String data) throws ParseException{
         SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
         Calendar calendar = Calendar.getInstance();
         Calendar calendarInserido = Calendar.getInstance();
         calendarInserido.setTime(formato.parse(data));
-        ZonedDateTime now = ZonedDateTime.now();
-        ZonedDateTime oldDate = now.minusDays(1).minusMinutes(10);
+        
         Duration duration = Duration.between(calendarInserido.getTime().toInstant(), calendar.getTime().toInstant());
-         return duration.toDays();
+        return duration.toDays();
     }
+    
+    public static String TempoDecorridoEmFormatoData(long dias) throws ParseException{
+        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+        boolean naoSair  = true;
+        int ano =0, mes = 0, dia = 0;
+        while(naoSair){
+            if(dias>=365){
+                ano++;
+                dias -= 365;
+            }else if(dias<365 && dias>=30){
+                mes++;
+                dias -= 30;
+            }else{
+                dia += dias;
+                naoSair = false;
+            }
+        }
+        return (dias+" dia(s) "+mes+" mes(es) "+ano+" ano(s)");
+    } 
 }
