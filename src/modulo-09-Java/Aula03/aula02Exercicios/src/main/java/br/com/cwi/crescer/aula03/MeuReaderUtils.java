7d42751187e4ed6febee7b0c5dad51ebd12aa305
Unknown <jonathanwssantos1@gmail.com>
2016-11-26 18:46:01 -0200

    feat(Java): adicionar exercicio 2 MeuReaderUtils.

---
+++ src/modulo-09-Java/Aula03/aula02Exercicios/src/main/java/br/com/cwi/crescer/aula03/MeuReaderUtils.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.cwi.crescer.aula03;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+/**
+ *
+ * @author Jonathan-David
+ */
+public class MeuReaderUtils {
+    public void LeitorDeArquivos(String arquivo){
+         try (   
+             Reader reader = new FileReader(arquivo);
+             BufferedReader bufferReader = new BufferedReader(reader);
+            ){
+                bufferReader.lines()
+                     .forEach(System.out::println);
+            }catch(IOException e){
+                System.err.println("Arquivo não encontrado ou não existe");
+            }
+    }
+}
