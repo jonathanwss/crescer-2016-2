0efb82f19b43beed437c67c744760b2fe8781c35
Unknown <jonathanwssantos1@gmail.com>
2016-11-27 01:25:02 -0200

    fix(Java): corrigir nome package exercicios aula 2.

--- src/modulo-09-Java/Aula03/aula02Exercicios/src/main/java/br/com/cwi/crescer/aula02/MeuReaderUtils.java
+++ src/modulo-09-Java/Aula03/aula02Exercicios/src/main/java/br/com/cwi/crescer/aula02/MeuReaderUtils.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.com.cwi.crescer.aula03;
+package br.com.cwi.crescer.aula02;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
