cdb0230ff4b9fc074b00562ec3150a9b35576e37
Unknown <jonathanwssantos1@gmail.com>
2016-11-28 22:08:54 -0200

    feat(Java): adicionar classe de mapeamento para contract_value.

---
+++ src/modulo-09-Java/Aula3/Aula03/src/main/java/br/com/cwi/crescer/aula03/entity/Contract_Value.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.cwi.crescer.aula03.entity;
+import java.io.Serializable;
+import javax.persistence.Basic;
+import static javax.persistence.CascadeType.ALL;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.SEQUENCE;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+/**
+ *
+ * @author Jonathan-David
+ */
+@Entity
+@Table(name="Contract_Value")
+public class Contract_Value implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = SEQUENCE, generator = "SEQ_CONTRACT_VALUE")
+    @SequenceGenerator(name = "SEQ_CONTRACT_VALUE", sequenceName = "SEQ_CONTRACT_VALUE", allocationSize = 1)
+    @Basic(optional = false)
+    @Column(name = "ID_CONTRACT_VALUE")
+    private Long idContractValue;
+    
+    @Basic(optional = false)
+    @Column(name = "DS_COIN")
+    private String coin;
+    
+    @Basic(optional = false)
+    @Column(name = "DS_PERIODICITY")
+    private String periodicity;
+    
+    @Basic(optional = false)
+    @Column(name = "VL_AMOUNT_CONTRACT_VALUE")
+    private Long amountContractValue;
+    
+    @Basic(optional = false)
+    @Column(name = "VL_MONTHLY_USD")
+    private Long monthlyUsd;
+    
+    @OneToMany(cascade = ALL)
+    private Contract contract;
+
+    public Long getIdContractValue() {
+        return idContractValue;
+    }
+
+    public void setIdContractValue(Long idContractValue) {
+        this.idContractValue = idContractValue;
+    }
+
+    public String getCoin() {
+        return coin;
+    }
+
+    public void setCoin(String coin) {
+        this.coin = coin;
+    }
+
+    public String getPeriodicity() {
+        return periodicity;
+    }
+
+    public void setPeriodicity(String periodicity) {
+        this.periodicity = periodicity;
+    }
+
+    public Long getAmountContractValue() {
+        return amountContractValue;
+    }
+
+    public void setAmountContractValue(Long amountContractValue) {
+        this.amountContractValue = amountContractValue;
+    }
+
+    public Long getMonthlyUsd() {
+        return monthlyUsd;
+    }
+
+    public void setMonthlyUsd(Long monthlyUsd) {
+        this.monthlyUsd = monthlyUsd;
+    }
+
+    public Contract getContract() {
+        return contract;
+    }
+
+    public void setContract(Contract contract) {
+        this.contract = contract;
+    }
+    
+    
+}
